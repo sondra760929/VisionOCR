71d267ae7da3f694a5d05be9aad9675d502a2fb0
sondra76 <sondra76@gmail.com>
2020-01-08 07:22:43 +0900

    1. 다단 영역 지정(세로 1, 세로2, 세로 3) 3. 영역 지정 일괄 리셋(모두 삭제) 4. 추출결과와 기준 파일(미리보기) 링크 (테이블에서 해당 항목 선택시 해당 파일로 이동) 5. 엑셀로 저장된 추출 결과물 A열에 파일 네임 삽입. (현재는 추출된 영역 결과물만 나옴) 8. 진행상황 프로그래스 창 표시

---
+++ DBVisionOCR/DlgProgress.cpp
@@ -0,0 +1,34 @@
+﻿// DlgProgress.cpp: 구현 파일
+//
+#include "stdafx.h"
+#include "DlgProgress.h"
+#include "afxdialogex.h"
+#include "resource.h"
+
+// CDlgProgress 대화 상자
+
+IMPLEMENT_DYNAMIC(CDlgProgress, CDialog)
+
+CDlgProgress::CDlgProgress(CWnd* pParent /*=nullptr*/)
+	: CDialog(IDD_DIALOG_PROGRESS, pParent)
+{
+
+}
+
+CDlgProgress::~CDlgProgress()
+{
+}
+
+void CDlgProgress::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_STATIC_STATUS, m_stStatus);
+	DDX_Control(pDX, IDC_PROGRESS_STATUS, m_progressBar);
+}
+
+
+BEGIN_MESSAGE_MAP(CDlgProgress, CDialog)
+END_MESSAGE_MAP()
+
+
+// CDlgProgress 메시지 처리기
---
+++ DBVisionOCR/DlgProgress.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+
+// CDlgProgress 대화 상자
+
+class CDlgProgress : public CDialog
+{
+	DECLARE_DYNAMIC(CDlgProgress)
+
+public:
+	CDlgProgress(CWnd* pParent = nullptr);   // 표준 생성자입니다.
+	virtual ~CDlgProgress();
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_DIALOG_PROGRESS };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+	DECLARE_MESSAGE_MAP()
+public:
+	CStatic m_stStatus;
+	CProgressCtrl m_progressBar;
+};
